957febec1dc34da265e5efedd47ac3c4c69ee6d1
Ernerst <ernerstm@gmail.com>
2017-10-13 15:38:52 +0200

    Initial commit

---
+++ app/src/main/java/com/ernerst/mapotse/tembisatourguide/HomeFragment.java
@@ -0,0 +1,23 @@
+package com.ernerst.mapotse.tembisatourguide;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by eKasiLab Alex CDTB on 2017/10/13.
+ */
+
+public class HomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.item_home, container, false);
+        return view;
+    }
+}
